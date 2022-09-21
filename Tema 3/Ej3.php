8afbe6d8491834830f220c85163f4d9aa14638a8
Iago Ortiz <iago.ortiz-duran@iesruizgijon.com>
2022-09-21 12:42:08 +0200

    Ejercicio 3

---
+++ Tema 3/Ej3.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Configura tu página web</h1>
+    <form action="Ej3Pagina.php">
+        <ul>
+            <li>Color de fondo&nbsp;&nbsp;&nbsp;&nbsp;<input type="color" name="BG"></li><br>
+            <li>Tipo de letra&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<select name="fuente">
+                <option value="times new roman">Times New Roman</option>
+                <option value="arial">Arial</option>
+                <option value="verdana">Verdana</option>
+                <option value="tahoma">Tahoma</option>
+            </select></li><br>
+            <li>Alineación de texto&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<select name="fuente">
+                <option value="derecha">Derecha</option>
+                <option value="centrado">Centrado</option>
+                <option value="izquierda">Izquierda</option>
+            </select></li><br>
+            <li>Banner&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<select name="fuente">
+                <option value="derecha">Derecha</option>
+                <option value="centrado">Centrado</option>
+                <option value="izquierda">Izquierda</option>
+            </select></li><br>
+        </ul>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="submit" value="Generar">
+    </form>
+</body>
+</html>
